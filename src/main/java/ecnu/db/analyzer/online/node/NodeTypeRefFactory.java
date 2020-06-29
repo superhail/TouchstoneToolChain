45aaa9ac9127e1727e80fe47f969e0bcdb2307da
locust <hello@localhost>
2020-06-29 21:17:22 +0800

    refactor:merge Tidb3NodeTypeTool and Tidb4NodeTypeTool together

--- src/main/java/ecnu/db/analyzer/online/node/NodeTypeRefFactory.java
+++ src/main/java/ecnu/db/analyzer/online/node/NodeTypeRefFactory.java
@@ -2,12 +2,6 @@ package ecnu.db.analyzer.online.node;
 
 public class NodeTypeRefFactory {
     public static NodeTypeTool getNodeTypeRef(String tidbVersion) {
-        if ("3.1.0".equals(tidbVersion)) {
-            return new Tidb3NodeTypeTool();
-        } else if ("4.0.0".equals(tidbVersion)) {
-            return new Tidb4NodeTypeTool();
-        }
-
-        return null;
+        return new TidbNodeTypeTool();
     }
 }
--- src/main/java/ecnu/db/analyzer/online/node/TidbNodeTypeTool.java
+++ src/main/java/ecnu/db/analyzer/online/node/TidbNodeTypeTool.java
@@ -4,13 +4,13 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.HashSet;
 
-class Tidb3NodeTypeTool implements NodeTypeTool {
+class TidbNodeTypeTool implements NodeTypeTool {
     private static final HashSet<String> readerNodeTypes = new HashSet<>(Arrays.asList("TableReader", "IndexReader", "IndexLookUp"));
-    private static final HashSet<String> passNodeTypes = new HashSet<>(Arrays.asList("Projection", "TopN", "Sort", "HashAgg", "StreamAgg", "IndexScan"));
-    private static final HashSet<String> joinNodeTypes = new HashSet<>(Arrays.asList("HashRightJoin", "HashLeftJoin", "IndexMergeJoin", "IndexHashJoin", "IndexJoin", "MergeJoin"));
+    private static final HashSet<String> passNodeTypes = new HashSet<>(Arrays.asList("Projection", "TopN", "Sort", "HashAgg", "StreamAgg", "IndexScan", "IndexRangeScan", "IndexFullScan"));
+    private static final HashSet<String> joinNodeTypes = new HashSet<>(Arrays.asList("HashRightJoin", "HashLeftJoin", "IndexMergeJoin", "IndexHashJoin", "IndexJoin", "MergeJoin","HashJoin"));
     private static final HashSet<String> filterNodeTypes = new HashSet<>(Collections.singletonList("Selection"));
-    private static final HashSet<String> tableScanNodeTypes = new HashSet<>(Collections.singletonList("TableScan"));
-    private static final HashSet<String> indexScanNodeTypes = new HashSet<>(Collections.singletonList("IndexScan"));
+    private static final HashSet<String> tableScanNodeTypes = new HashSet<>(Arrays.asList("TableScan", "TableFullScan", "TableRowIDScan", "TableRangeScan"));
+    private static final HashSet<String> indexScanNodeTypes = new HashSet<>(Arrays.asList("IndexScan", "IndexRangeScan", "IndexFullScan"));
     @Override
     public boolean isReaderNode(String nodeType) {
         return readerNodeTypes.contains(nodeType);
