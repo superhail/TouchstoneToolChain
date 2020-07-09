d67bcb807bd6d58f2ae4319067d0806017cfbad9
locust <hello@localhost>
2020-07-09 16:19:43 +0800

    refactor:rename arithmetic operators to inline with tidb operator names

--- src/main/java/ecnu/db/constraintchain/arithmetic/operator/MinusOperator.java
+++ src/main/java/ecnu/db/constraintchain/arithmetic/operator/MinusOperator.java
@@ -5,15 +5,15 @@ import ecnu.db.constraintchain.arithmetic.ArithmeticNode;
 /**
  * @author wangqingshuai
  */
-public class AddOperator extends AbstractArithmeticOperator {
-    public AddOperator(ArithmeticNode leftNode, ArithmeticNode rightNode) {
+public class MinusOperator extends AbstractArithmeticOperator {
+    public MinusOperator(ArithmeticNode leftNode, ArithmeticNode rightNode) {
         super(leftNode, rightNode);
     }
 
     @Override
     float[] getValue(float[] leftValue, float[] rightValue) {
         for (int i = 0; i < leftValue.length; i++) {
-            leftValue[i] += rightValue[i];
+            leftValue[i] -= rightValue[i];
         }
         return leftValue;
     }
